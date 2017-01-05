ecf147580fbb211e5dae0b759451093eb898faf4
zhangxb12370 <2104605213@qq.com>
2017-01-05 14:12:12 +0800

    test

---
+++ main/java/com/kevin/IversonApplication.java
@@ -0,0 +1,11 @@
+package com.kevin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IversonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IversonApplication.class, args);
+	}
+}
